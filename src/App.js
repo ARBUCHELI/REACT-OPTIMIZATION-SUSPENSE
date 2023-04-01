53f9e38b4ca6899557c2eae96985a284b217446d
ARBUCHELI <radio_seattle@hotmail.com>
2023-04-01 16:06:21 -0500

    creating the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import './styles.css';
+import { Loader } from './Loader';
 
-function App() {
+const Details = React.lazy(() => {
+  // Artificially making loadind the <Details> component take 1 second
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve(import('./Details')), 1000);
+  });
+});
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <h1 style={{textAlign: "center", paddingTop: "1em", fontFamily: "monospace", fontWeight: "bold", fontSize: "3em", color: "white"}}>React Optimization: Suspense and React.lazy</h1>
+        <div className='e7-container' style={{width: "50%", margin: "auto", paddingTop: "2%"}}>
+          <div className='e7-content-container'>
+            <img src={require('./logo.png')} alt='Restaurant logo' />
+            <h1>Your reservation is confirmed.</h1>
+          </div>
+          <Suspense fallback={Loader}>
+            <Details />
+          </Suspense>
+        </div>
+      </div>
+      <footer style={{textAlign: "center", paddingTop: "2em", paddingBottom: "2em", fontFamily: "monospace", fontWeight: "bold", fontSize: "2em", color: "white"}}>Bucheli Web © 2023</footer>
+    </>
   );
-}
+};
 
-export default App;
+export default App
